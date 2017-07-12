d95b9c0aeff47323820ae4b142da14fed1bbe060
diegosdlc <diegosdlc@users.noreply.github.com>
2017-07-12 21:12:22 +0200

    Archivos nuevos

---
+++ Spotify_launch.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+import webbrowser
+
+#
+route = ""
+route = raw_input(““)
+#ejecuta el
+os.system('start "D:\\PROGRAMAS\\Steam\\Steam.exe"')
---
+++ chrome_Netflix_launch.py
@@ -0,0 +1,7 @@
+import os
+import subprocess
+import webbrowser
+
+chrome = r"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+
+os.system('start chrome "https://netflix.com" --new-tab')
