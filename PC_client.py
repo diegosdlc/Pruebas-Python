047adc40286988b3fd020b6e58ca339515b20667
diegosdlc <diegosdlc@users.noreply.github.com>
2017-07-15 21:01:10 +0200

    pruebas server/client

--- PC_client.py
+++ PC_client.py
@@ -1,39 +1,29 @@
 import socket
 import sys
+import time
 
-def get_constants(prefix):
-    """Create a dictionary mapping socket module constants to their names."""
-    return dict( (getattr(socket, n), n)
-                 for n in dir(socket)
-                 if n.startswith(prefix)
-                 )
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-families = get_constants('AF_')
-types = get_constants('SOCK_')
-protocols = get_constants('IPPROTO_')
-
-# Create a TCP/IP socket
-sock = socket.create_connection(('localhost', 10000))
-
-print >>sys.stderr, 'Family  :', families[sock.family]
-print >>sys.stderr, 'Type    :', types[sock.type]
-print >>sys.stderr, 'Protocol:', protocols[sock.proto]
-print >>sys.stderr
+server_address = ('localhost', 10000)
+message = 'This is the message.  It will be repeated.'
 
 try:
+
     # Send data
-    message = 'This is the message.  It will be repeated.'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+    sys.stderr.write('sending "%s"' % message)
+    #print >>sys.stderr, 'sending "%s"' % message
+    sent = sock.sendto(message.encode('utf-8'), server_address)
+    time.sleep(6)
 
-    amount_received = 0
-    amount_expected = len(message)
-
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
+    # Receive response
+    sys.stderr.write("waiting to receive")
+    #print >>sys.stderr, 'waiting to receive'
+    data, server = sock.recvfrom(4096)
+    sys.stderr.write('received "%s"' % data)
+    #print >>sys.stderr, 'received "%s"' % data
 
 finally:
-    print >>sys.stderr, 'closing socket'
+    sys.stderr.write('closing socket')
+    #print >>sys.stderr, 'closing socket'
     sock.close()
--- PC_server.py
+++ PC_server.py
@@ -1,36 +1,28 @@
 import socket
 import sys
+import time
 
 # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the port
 server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+sys.stderr.write('starting up on %s port %s' % (server_address))
+#print('starting up on %s port %s' % (server_address)) #>> sys.stderr
 sock.bind(server_address)
 
-# Listen for incoming connections
-sock.listen(1)
-
 while True:
-    # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
+    sys.stderr.write('\nwaiting to receive message')
+    #print >>sys.stderr, '\nwaiting to receive message'
+    data, address = sock.recvfrom(4096)
 
- try:
-        print >>sys.stderr, 'connection from', client_address
+    sys.stderr.write('received %s bytes from %s' % (len(data), address))
+    time.sleep(6)
+    #print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+    sys.stderr.write(data)
+    #print >>sys.stderr, data
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
-            if data:
-                print >>sys.stderr, 'sending data back to the client'
-                connection.sendall(data)
-            else:
-                print >>sys.stderr, 'no more data from', client_address
-                break
-
-    finally:
-        # Clean up the connection
-        connection.close()
+    if data:
+        sent = sock.sendto(data, address)
+        sys.stderr.write('sent %s bytes back to %s' % (sent, address))
+        #print >>sys.stderr, 'sent %s bytes back to %s' % (sent, address)
